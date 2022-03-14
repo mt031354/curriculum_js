1f01432665f7a602765b651acfccfeb07077319c
mt031354 <100955716+mt031354@users.noreply.github.com>
2022-03-14 23:55:22 +0900

    課題の提出

--- JavaScript/Check1/main.js
+++ JavaScript/Check1/main.js
@@ -1,11 +1,17 @@
 //問1:isEven関数を実行して、以下の配列から偶数だけが出力されるように実装してください。
 let numbers = [2, 5, 12, 13, 15, 18, 22];
+console.log(numbers);
 
-function isEven(num) {
-    console.log(num + 'は偶数です');
+function isEven(){
+    for( i = 0; i <= numbers.length; i++){
+        if(numbers[i] % 2 === 0){
+            console.log(numbers[i]+ 'は偶数です');
+    }
+    }
 }
 
-let number = isEven('2,12,18,22');
+let num = isEven(numbers)
+ 
 
 
 //問2:以下の要件を満たすように実装してください。
